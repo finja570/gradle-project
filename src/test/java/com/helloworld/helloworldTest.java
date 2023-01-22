65a7dcdd714214c24789e91ea6ddd8c19607d786
Finja Wegener <73483284+finja570@users.noreply.github.com>
2023-01-22 14:10:33 +0100

    change folder struct

---
+++ src/test/java/com/helloworld/helloworldTest.java
@@ -0,0 +1,11 @@
+package com.helloworld;
+
+import org.junit.Test;
+
+public class helloworldTest {
+
+  @Test
+  public void verifyNoExceptionThrown() {
+    helloworld.main(new String[] {});
+  }
+}
